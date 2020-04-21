eee2ca932a738adb805daa2e2d529ada998fb483
Minh Trinh <mtrinh@bu.edu>
2020-04-21 17:36:24 -0400

    Fixed no open order bug

--- Square/controllers/paymentController.js
+++ Square/controllers/paymentController.js
@@ -26,21 +26,20 @@ exports.pay_order = async function(req, res) {
   const request_params = req.body;
   const idempotency_key = crypto.randomBytes(22).toString('hex');
   //Query DB to find open orders
-  const toPay = await Order.findOne({"state": "OPEN"}, function(order) {
-    if(order == null) res.status(404).send("No open orders")
-  })
-  
-  const request_body = {
-		source_id: request_params.nonce,
-		amount_money: {
-			amount: toPay.total_money.amount,
-			currency: 'USD',
-    },
-    order_id: toPay.order_id,
-		idempotency_key: idempotency_key,
-	};
 
 	try {
+		const toPay = await Order.findOne({"state": "OPEN"}, function(order, error) {
+			if (order == null) return error
+		})
+		const request_body = {
+			source_id: request_params.nonce,
+			amount_money: {
+				amount: toPay.total_money.amount,
+				currency: 'USD',
+			},
+			order_id: toPay.order_id,
+			idempotency_key: idempotency_key,
+		};
     const response = await payments_api.createPayment(request_body);
     //Update status of order after its paid
     await Order.updateOne({"state": "OPEN"}, {"state": "COMPLETED"})
@@ -49,9 +48,6 @@ exports.pay_order = async function(req, res) {
 			result: response,
 		});
 	} catch (error) {
-		res.status(500).json({
-			title: 'Payment Failure',
-			result: error.response.text,
-		});
+		res.status(500).send('No open orders')
 	}
 }
